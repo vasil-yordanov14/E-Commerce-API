f106aac7606137508a74b17a017372442b7ed491
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-23 23:17:17 +0200

    morgan package

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ require("express-async-errors");
 // express
 const express = require("express");
 const app = express();
+
+// rest of the packages
+const morgan = require("morgan");
+
 // database
 const connectDB = require("./db/connect");
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
