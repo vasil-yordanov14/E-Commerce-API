13a5791c240131b13095b648d1f1af3786ebf9eb
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-23 22:32:33 +0200

    basic routes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 require("dotenv").config();
+require("express-async-errors");
 // express
 const express = require("express");
 const app = express();
 // database
 const connectDB = require("./db/connect");
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("e-commerce-api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
