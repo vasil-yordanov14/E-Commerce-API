cc02e7cb18a7cee404d0cb4e3fab10647ff06f0f
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-26 18:31:06 +0200

    register unique email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const customError = require("../errors/index");
+const CustomError = require("../errors/index");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
