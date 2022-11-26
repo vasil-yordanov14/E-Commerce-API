900397a523d1c4136023c9088995c90a0f623df9
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-26 20:11:53 +0200

    user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,16 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors/index");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, name, password } = req.body;
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError("Email already exists");
   }
-  const user = await User.create(req.body);
+  // first registered user is admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
