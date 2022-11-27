fc37f5036227b27643fd2099aad71754d3134803
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-27 15:44:59 +0200

    refactor cookies setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors/index");
-const { createJWT, isTokenValid } = require("../utils");
+const { attachCookiesToResponse } = require("../utils");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,15 +15,7 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password, role });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  // const token = createJWT({ payload: tokenUser });
-  const token = createJWT({ payload: tokenUser });
-
-  const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-  });
-
+  attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
