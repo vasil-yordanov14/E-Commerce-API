a9f7ce5f414fd7190ab0d86564d0fcc8c89bcbfa
vasil-yordanov14 <vasil_sh@abv.bg>
2022-12-01 21:23:38 +0200

    updatePassword controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,18 @@ const updateUser = async (req, res) => {
   res.send(req.body);
 };
 const updateUserPassword = async (req, res) => {
-  res.send(req.body);
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError("Please provide both values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  user.password = newPassword;
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Success! Password updated" });
 };
 
 module.exports = {
