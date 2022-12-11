df719da83e1ee58554a5a19d2eeaa43500d11c14
vasil-yordanov14 <vasil_sh@abv.bg>
2022-12-11 14:42:39 +0200

    updateUser controller .save()

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,11 +23,13 @@ const updateUser = async (req, res) => {
   if (!email || !name) {
     throw new CustomError.BadRequestError("Please provide all values");
   }
-  const user = await User.findOneAndUpdate(
-    { _id: req.user.userId },
-    { email, name },
-    { new: true, runValidators: true }
-  );
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+
+  await user.save();
+
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.OK).json({ user: tokenUser });
