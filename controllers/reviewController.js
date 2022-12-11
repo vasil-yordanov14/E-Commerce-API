5c6cb64021fba71ba1dc0e1978a72031034dab09
vasil-yordanov14 <vasil_sh@abv.bg>
2022-12-11 23:19:10 +0200

    review routes structure

---
+++ controllers/reviewController.js
@@ -0,0 +1,23 @@
+const createReview = async (req, res) => {
+  console.log("create review");
+};
+const getAllReviews = async (req, res) => {
+  console.log("get all reviews");
+};
+const getSingleReview = async (req, res) => {
+  console.log("get single review");
+};
+const updateReview = async (req, res) => {
+  console.log("update review");
+};
+const deleteReview = async (req, res) => {
+  console.log("delete review");
+};
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+};
