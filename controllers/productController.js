f3e51ef374496c9a52ffb8e84343cb66ed2f26cd
vasil-yordanov14 <vasil_sh@abv.bg>
2022-12-11 22:24:45 +0200

    uploadImage controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const path = require("path");
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -50,7 +51,29 @@ const deleteProduct = async (req, res) => {
 };
 
 const uploadImage = async (req, res) => {
-  res.send("upload image");
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload image smaller than 1MB"
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ img: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
